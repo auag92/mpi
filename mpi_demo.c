c045594b72006d74410c330e2c4efd2f13489711
auag92 <apaar92@gmail.com>
2015-07-17 13:11:23 +0530

    factorial mpi

--- mpi_demo.c
+++ mpi_demo.c
@@ -1,195 +1,225 @@
 #include <mpi.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+#include "stdio.h"
+#include "stdlib.h"
+#include "math.h"
+
 #define MESHX 100
 #define MASTER 0
 #define NONE 0
 
 #define BEGIN 999
-#define LTAG 777
-#define RTAG 666
+#define LTAG  777
+#define RTAG  666
 #define WRITE 555
 
 #define ntimesteps 10000
 #define saveT 500
 
 #define D 1.0
-#define deltax 1
-#define deltat 1
+#define deltax 1.0
+#define deltat 0.1
+
+double *comp;
+long start, end;
 
 int numtasks, taskid;
 int numworkers;
 
-double *comp;
-long start, end, rows, t;
-int avgrow, extra, dest, rank, offset;
-int left_node, right_node;
 int source, msgtype;
-int dest;
+
+int averow, extra, rank, offset, dest;
+int left_node, right_node;
 MPI_Status status;
 
-void mpiexchange(int taskid){
-  if((taskid%2)==0){
-    if(taskid != numworkers){
-      MPI_Send(&comp[end],        1, MPI_DOUBLE, right_node, LTAG,    MPI_COMM_WORLD);
+long i, t,rows;
+
+FILE *fp;
+
+void mpiexchange(int taskid) {
+  if ((taskid%2) == 0) {
+    if (taskid != (numworkers)) {
+      MPI_Send(&comp[end],    1, MPI_DOUBLE,  right_node, LTAG,     MPI_COMM_WORLD);
       source  = right_node;
       msgtype = RTAG;
-      MPI_Recv(&comp[end+1],      1, MPI_DOUBLE, source,     msgtype, MPI_COMM_WORLD, &status);
+      MPI_Recv(&comp[end+1],  1, MPI_DOUBLE,  source,     msgtype,  MPI_COMM_WORLD, &status);
     }
-    MPI_Send(&comp[start],        1, MPI_DOUBLE, left_node,  RTAG,    MPI_COMM_WORLD);
+    MPI_Send(&comp[start],    1, MPI_DOUBLE,  left_node,  RTAG,     MPI_COMM_WORLD);
+    source  = left_node;
     msgtype = LTAG;
-    MPI_Recv(&comp[0],            1, MPI_DOUBLE, source,     msgtype, MPI_COMM_WORLD, &status);
-  }
-  else{
-    if(taskid != 1){
-      source  = left_node;
-      msgtype = LTAG;
-      MPI_Recv(&comp[0],          1, MPI_DOUBLE, source,    msgtype,  MPI_COMM_WORLD, &status);
-      MPI_Send(&comp[start],      1, MPI_DOUBLE, left_node, RTAG,     MPI_COMM_WORLD);
+    MPI_Recv(&comp[0],        1, MPI_DOUBLE,  source,     msgtype,  MPI_COMM_WORLD, &status);
+  } else {
+    if (taskid != 1) {
+       source  = left_node;
+       msgtype = LTAG;
+       MPI_Recv(&comp[0],     1, MPI_DOUBLE, source,      msgtype,  MPI_COMM_WORLD, &status);
+       MPI_Send(&comp[start], 1, MPI_DOUBLE, left_node,   RTAG,     MPI_COMM_WORLD);
     }
-    if(taskid != numworkers){
+    if (taskid != numworkers) {
       source  = right_node;
       msgtype = RTAG;
-      MPI_Recv(&comp[end+1],      1, MPI_DOUBLE, source,    msgtype,   MPI_COMM_WORLD, &status);
-      MPI_Send(&comp[start],      1, MPI_DOUBLE, left_node, RTAG,      MPI_COMM_WORLD);
+      MPI_Recv(&comp[end+1],  1, MPI_DOUBLE, source,      msgtype,  MPI_COMM_WORLD, &status);
+      MPI_Send(&comp[end],    1, MPI_DOUBLE, right_node,  LTAG,     MPI_COMM_WORLD);
     }
-  }
-}
-void sendtomaster(){
-  MPI_Send(&offset,     1, MPI_INT, dest, WRITE, MPI_COMM_WORLD);
-  MPI_Send(&rows,       1, MPI_INT, dest, WRITE, MPI_COMM_WORLD);
-  MPI_Send(&left_node,  1, MPI_INT, dest, WRITE, MPI_COMM_WORLD);
-  MPI_Send(&right_node, 1, MPI_INT, dest, WRITE, MPI_COMM_WORLD);
-  if(taskid == 1){
-    MPI_Send(&comp[0],  rows, MPI_DOUBLE, dest, BEGIN, MPI_COMM_WORLD);
-  }else{
-    MPI_Send(&comp[1],  rows, MPI_DOUBLE, dest, BEGIN, MPI_COMM_WORLD);
-  }
-}
-void initialize(){
-  long i;
-  for(i=0; i<MESHX; i++){
-    if(i<MESHX/2)
-      comp[i]=1.0;
-    else
-      comp[i]=0.0;
-  }
-}
-void receivefrmworker(){
-  int rank;
-  for(rank = 1; rank <= numworkers; rank++){
-    source  =rank;
-    MPI_Recv(&offset,     1, MPI_INT,          source, WRITE, MPI_COMM_WORLD, &status);
-    MPI_Recv(&rows,       1, MPI_INT,          source, WRITE, MPI_COMM_WORLD, &status);
-    MPI_Recv(&left_node,  1, MPI_INT,          source, WRITE, MPI_COMM_WORLD, &status);
-    MPI_Recv(&right_node, 1, MPI_INT,          source, WRITE, MPI_COMM_WORLD, &status);
-    MPI_Recv(&comp[offset], rows, MPI_DOUBLE,  source, WRITE, MPI_COMM_WORLD, &status);
-  }
-}
-void solverloop(){
-  double flux_back, flux_front;
-  long i;
-  flux_back = (comp[1]-comp[0])/deltax;
-  for(i=start; i<=end; i++){
-    flux_front     = (comp[i+1]-comp[i])/deltax;
-    comp[i]        = comp[i]+deltat*D*(flux_front - flux_back)/deltax;
-    flux_back      = flux_front;
   }
 }
 
-void writetofile(long t){
-  char name[1000];
+void sendtomaster(int taskid) {
+  dest = MASTER;
+
+  MPI_Send(&offset,       1,    MPI_INT,         dest, WRITE, MPI_COMM_WORLD);
+  MPI_Send(&rows,         1,    MPI_INT,         dest, WRITE, MPI_COMM_WORLD);
+  MPI_Send(&left_node,    1,    MPI_INT,         dest, WRITE, MPI_COMM_WORLD);
+  MPI_Send(&right_node,   1,    MPI_INT,         dest, WRITE, MPI_COMM_WORLD);
+  if (taskid == 1) {
+    MPI_Send(&comp[0], rows, MPI_DOUBLE,         dest, WRITE, MPI_COMM_WORLD);
+  } else {
+    MPI_Send(&comp[1], rows, MPI_DOUBLE,         dest, WRITE, MPI_COMM_WORLD);
+  }
+}
+
+void receivefrmworker() {
+  int rank;
+  for (rank=1; rank <= numworkers; rank++) {
+    source = rank;
+    MPI_Recv(&offset,       1,       MPI_INT,         source, WRITE, MPI_COMM_WORLD, &status);
+    MPI_Recv(&rows,         1,       MPI_INT,         source, WRITE, MPI_COMM_WORLD, &status);
+    MPI_Recv(&left_node,    1,       MPI_INT,         source, WRITE, MPI_COMM_WORLD, &status);
+    MPI_Recv(&right_node,   1,       MPI_INT,         source, WRITE, MPI_COMM_WORLD, &status);
+    MPI_Recv(&comp[offset], rows,    MPI_DOUBLE,      source, WRITE, MPI_COMM_WORLD, &status);
+  }
+}
+
+void solverloop(double *comp) {
+  double flux_back, flux_front;
   long i;
-  sprintf(name,"composition_%ld.dat",t);
-  FILE *fp = fopen(name,"w");
-  for(i = 0; i < MESHX; i++){
-    fprintf(fp,"%ld %le\n",i*deltax, comp[i]);
+  flux_back = (comp[1] - comp[0])/deltax;
+  for (i=start; i <= end; i++) {
+    flux_front = (comp[i+1] - comp[i])/deltax;
+    comp[i]    = comp[i]    + deltat*D*(flux_front - flux_back)/deltax;
+    flux_back  = flux_front;
+  }
+}
+
+void writetofile(long t) {
+  char name[1000];
+  sprintf(name, "composition_%ld.dat",t);
+  fp = fopen(name, "w");
+  for (i=0; i < (MESHX); i++) {
+    fprintf(fp, "%le %le\n", i*deltax, comp[i]);
   }
   fclose(fp);
 }
-void apply_boundary_conditions(){
-  if(taskid == 0){
-    comp[0] = comp[1];
+
+void apply_boundary_conditions(int taskid) {
+  if(taskid == 1) {
+    comp[0]   = comp[1];
   }
-  else if(taskid == numworkers){
-    comp[end + 1] = comp[end];
+  if(taskid == numworkers) {
+    comp[end+1] = comp[end];
   }
 }
 
-int main( int argc, char *argv[]){
-  initialize();
+void initialize() {
+  long i;
+  for (i=0; i < MESHX; i++) {
+    if(i < (MESHX/2)) {
+      comp[i] = 1.0;
+    } else {
+      comp[i] = 0.0;
+    }
+  }
+}
+
+void main(int argc, char *argv[]) {
+
   MPI_Init(&argc,&argv);
   MPI_Comm_size(MPI_COMM_WORLD,&numtasks);
   MPI_Comm_rank(MPI_COMM_WORLD,&taskid);
 
-  numworkers = numtasks - 1;
-  if(taskid == MASTER){
+  numworkers = numtasks-1;
 
+
+  if (taskid == MASTER) {
     comp = (double *)malloc(MESHX*sizeof(double));
 
-    avgrow = MESHX/numworkers;
-    extra  = MESHX%numworkers;
+    initialize();
+
+    averow  = MESHX/numworkers;
+    extra   = MESHX%numworkers;
+
     offset = 0;
-    for (rank = 1; rank <=numworkers; rank++){
-      rows = (rank <= extra) ? avgrow+1 : avgrow;
-      if (rank == 1)
-      left_node  =  NONE;
-      if (left_node == (numworkers)){
+
+     for (rank=1; rank <= (numworkers); rank++) {
+      rows = (rank <= extra) ? averow+1 : averow;
+
+      left_node  = rank - 1;
+      right_node = rank + 1;
+
+      if(rank == 1) {
+        left_node  = NONE;
+      }
+
+      if(rank == (numworkers)) {
         right_node = NONE;
       }
 
-      MPI_Send(&offset,     1, MPI_INT, dest, BEGIN, MPI_COMM_WORLD);
-      MPI_Send(&rows,       1, MPI_INT, dest, BEGIN, MPI_COMM_WORLD);
-      MPI_Send(&left_node,  1, MPI_INT, dest, BEGIN, MPI_COMM_WORLD);
-      MPI_Send(&right_node, 1, MPI_INT, dest, BEGIN, MPI_COMM_WORLD);
+      dest = rank;
 
-      MPI_Send(&comp[offset],  rows, MPI_DOUBLE, dest, BEGIN, MPI_COMM_WORLD);
+      MPI_Send(&offset,       1,    MPI_INT,         dest, BEGIN, MPI_COMM_WORLD);
+      MPI_Send(&rows,         1,    MPI_INT,         dest, BEGIN, MPI_COMM_WORLD);
+      MPI_Send(&left_node,    1,    MPI_INT,         dest, BEGIN, MPI_COMM_WORLD);
+      MPI_Send(&right_node,   1,    MPI_INT,         dest, BEGIN, MPI_COMM_WORLD);
+      MPI_Send(&comp[offset], rows, MPI_DOUBLE,      dest, BEGIN, MPI_COMM_WORLD);
+
       offset = offset + rows;
     }
-    for(t=1; t<ntimesteps; t++){
-      if (t%saveT == 0){
+
+    for (t=1; t < ntimesteps; t++) {
+      if (t%saveT == 0) {
         receivefrmworker();
         writetofile(t);
       }
     }
     free(comp);
   }
-  else {
-    source    = MASTER;
-    msgtype   = BEGIN;
-    MPI_Recv(&offset,     1, MPI_INT, source,msgtype,MPI_COMM_WORLD,&status);
-    MPI_Recv(&rows,       1, MPI_INT, source,msgtype,MPI_COMM_WORLD,&status);
-    MPI_Recv(&left_node,  1, MPI_INT, source,msgtype,MPI_COMM_WORLD,&status);
-    MPI_Recv(&right_node, 1, MPI_INT, source,msgtype,MPI_COMM_WORLD,&status);
+
+  if(taskid != MASTER) {
+    source  = MASTER;
+    msgtype = BEGIN;
+    MPI_Recv(&offset,     1, MPI_INT, source, msgtype, MPI_COMM_WORLD, &status);
+    MPI_Recv(&rows,       1, MPI_INT, source, msgtype, MPI_COMM_WORLD, &status);
+    MPI_Recv(&left_node,  1, MPI_INT, source, msgtype, MPI_COMM_WORLD, &status);
+    MPI_Recv(&right_node, 1, MPI_INT, source, msgtype, MPI_COMM_WORLD, &status);
+
 
     start = 1;
-    if (taskid == 1){
-      end = rows - 1;
+    if ((taskid == 1) || (taskid == numworkers)) {
       comp = (double *)malloc((rows+1)*sizeof(double));
-      MPI_Recv(&comp[0], rows, MPI_DOUBLE, source,msgtype,MPI_COMM_WORLD,&status);
-    }
-    else if(taskid == numworkers){
-      end = rows - 1;
-      comp = (double *)malloc((rows+1)*sizeof(double));
-      MPI_Recv(&comp[1], rows, MPI_DOUBLE, source,msgtype,MPI_COMM_WORLD,&status);
-    }
-    else{
-      end = rows;
+
+      if (taskid == 1) {
+        MPI_Recv(&comp[0], rows, MPI_DOUBLE, source, msgtype, MPI_COMM_WORLD, &status);
+      } else {
+        MPI_Recv(&comp[1], rows, MPI_DOUBLE, source, msgtype, MPI_COMM_WORLD, &status);
+      }
+      end   = rows-1;
+    } else {
       comp = (double *)malloc((rows+2)*sizeof(double));
-      MPI_Recv(&comp[1], rows, MPI_DOUBLE, source,msgtype,MPI_COMM_WORLD,&status);
+      MPI_Recv(&comp[1], rows, MPI_DOUBLE, source, msgtype, MPI_COMM_WORLD, &status);
+      end = rows;
     }
-    for(t=1; t<ntimesteps; t++){
+
+    long t;
+
+    for (t=1; t < ntimesteps; t++) {
       mpiexchange(taskid);
       solverloop(comp);
       apply_boundary_conditions(taskid);
-      if (t%saveT == 0){
-        sendtomaster();
+      if (t%saveT == 0) {
+        sendtomaster(taskid);
       }
     }
     free(comp);
   }
-  //printf("hello Apaar, No. of tasks = %d, My name = %d\n",numtasks, taskid);
-
+//   printf("Hello Computer, total no of processes = %d, my name is=%d\n",numtasks, taskid);
   MPI_Finalize();
 }
--- mpi_factorial.c
+++ mpi_factorial.c
@@ -3,55 +3,22 @@
 #include <stdlib.h>
 #include <math.h>
 #define MASTER 0
+#define BEGIN 111
+#define WRITE 222
+int my_rank;          // rank of process
+int numtasks;         // number of porcess
+int numworkers;       // no. of worker nodes
+int rank;
+int source;           // rank of sender
+int dest;             // rank of receiver
+int tag = 0;          // tag of message
+int message;          // storage for messages
+MPI_Status status;    // return status for receive
+long n;                // Input for factorial calculation
+long fact, ans;
+long start, end;
 
-int mult(int start, int end);
-void get_arg(int *n);
-int main(int argc, char *argv[]){
-  int my_rank;          // rank of process
-  int p;                // number of porcess
-  int source;           // rank of sender
-  int dest;             // rank of receiver
-  int tag = 0;          // tag of message
-  int message;          // storage for messages
-  MPI_Status status;    // return status for receive
-
-  int n;                // Input for factorial calculation
-  get_arg(&n);
-
-  /*Start up MPI*/
-  MPI_Init(&argc, &argv);
-  /*Find out the number of processes*/
-  MPI_Comm_size(MPI_COMM_WORLD, &p);
-
-  if(my_rank != 0){
-    /*Create Message*/
-    dest = 0;
-    int q = n/p;
-    int rem = n - q*p;
-    int start,end;
-    if (my_rank<p){
-      end = q*(my_rank-1) + 1;
-      start = q*my_rank;
-    }
-    else{
-      end = q*(my_rank-1) + 1;
-      start = n;
-    }
-    int message = mult(start, end);
-    MPI_Send(message, strlen(message)+1, MPI_CHAR,dest,tag, MPI_COMM_WORLD);
-  }
-  else{
-    for(source=1; source < p; source++){
-      MPI_Recv(message,100,MPI_CHAR,source,tag,MPI_COMM_WORLD,&status);
-      int fact = fact*message;
-      printf("factorial is %d\n",fact);
-    }
-  }
-  /*Shutdown MPI*/
-  MPI_Finalize();
-}
-
-int mult(int start, int end){
+long mult(long start, long end){
   int ans;
   if (start != end)
     return start*mult(start-1, end);
@@ -64,4 +31,48 @@ void get_arg(int *n){
   printf("enter the argument:");
   scanf("%d", n);
   printf("\n");
+}
+int main(int argc, char *argv[]){
+
+  /*Start up MPI*/
+  MPI_Init(&argc, &argv);
+  /*Find out the number of processes*/
+  MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+  numworkers  = numworkers - 1;
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+  if(my_rank == 0){
+    get_arg(&n);
+    int q, rem;
+    q   =   n/numworkers;
+    rem =   n%numworkers;
+    end    =    0;
+    start  =    q;
+    for (rank=1; rank <= (numworkers); rank++) {
+     dest   =    rank;
+     end    =    (long)(q*(my_rank-1));
+     start  =    (long)(q*my_rank - 1);
+     if(  rem >= my_rank ){
+       start++;
+     }
+     MPI_Send(&start,    1,    MPI_LONG,    dest,   BEGIN,  MPI_COMM_WORLD);
+     MPI_Send(&end,      1,    MPI_LONG,    dest,   BEGIN,  MPI_COMM_WORLD);
+   }
+   fact = 1;
+   for (rank=1; rank <= numworkers; rank++) {
+     source = rank;
+     MPI_Recv(&ans,      1,    MPI_LONG,    source, WRITE,  MPI_COMM_WORLD, &status);
+     fact = fact*ans;
+   }
+   printf("The factorial is:%ld",fact);
+  }
+  else{
+    source = MASTER;
+    MPI_Recv(&start,      1,   MPI_LONG,    source,  BEGIN,  MPI_COMM_WORLD, &status);
+    MPI_Recv(&end,        1,   MPI_LONG,    source,  BEGIN,  MPI_COMM_WORLD, &status);
+    ans = mult(start, end);
+    MPI_Send(&ans,        1,   MPI_LONG,    MASTER,   BEGIN,  MPI_COMM_WORLD);
+  }
+
+  /*Shutdown MPI*/
+  MPI_Finalize();
 }
